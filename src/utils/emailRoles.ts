dd5c08e23f781b6deca16b618fd69a8a0e6c9831
Jose Blanco <blancocasal@hotmail.com>
2025-01-08 10:46:33 +0100

    New EmailRoles

--- src/utils/emailRoles.ts
+++ src/utils/emailRoles.ts
@@ -1,8 +1,9 @@
 export const emailRoles = {
   "jose@blancoyenbatea.com": "admin",
   "manu@blancoyenbatea.com": "admin",
-  "admin@example.com": "admin",
   "miriam@mnprogram.com": "editor",
   "natalia@mnprogram.com": "editor",
-  "editor@example.com": "editor",
+  "gerardo@mnprogram.com": "editor",
+  "ventas@mnprogram.com": "editor",
+  "begona@mnprogram.com": "editor",
 };
